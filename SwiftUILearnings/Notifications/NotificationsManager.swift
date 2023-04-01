a939bb8a2e36748ab67595aafd1912c38cc4ef7a
Saurabh Verma <saurabh1088@gmail.com>
2023-04-01 08:17:33 +0530

    Started some notes for push notification.

--- SwiftUILearnings/Notifications/NotificationsManager.swift
+++ SwiftUILearnings/Notifications/NotificationsManager.swift
@@ -7,7 +7,21 @@
 // Copyright © 2023 Saurabh Verma, (saurabh1088@gmail.com). All rights reserved.
 //
         
-
+/**
+ User facing notifications play an important role in communicating users of apps important information about
+ some events they might be interested in or about some event in the app they might need to know about.
+ 
+ User notifications can be
+ - Local Notifications
+ - Remote Notifications
+ 
+ `Local Notifications`
+ App can generate notifications locally by specifying the content and condition for delivery like time or location which
+ will trigger the application.
+ 
+ `Remote Notifications`
+ Remote notifications are 
+ */
 import Foundation
 import UserNotifications
 
