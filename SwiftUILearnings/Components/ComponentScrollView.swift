515d02aac354c0a6fb6f761e801861dd4960e385
Saurabh Verma <saurabh1088@gmail.com>
2023-08-02 23:21:58 +0530

    Added some examples for new ScrollView APIs.

--- SwiftUILearnings/Components/ComponentScrollView.swift
+++ SwiftUILearnings/Components/ComponentScrollView.swift
@@ -8,18 +8,26 @@
 import SwiftUI
 
 struct ComponentScrollView: View {
+    @State private var showModal: Bool = false
+    var colors: [Color] = [.red, .green, .orange, .blue, .cyan, .yellow, .purple, .pink, .brown, .mint]
     var body: some View {
-        if #available(iOS 17.0, *) {
-            scrollViewIOS17Vertical
-        } else {
-            scrollViewPreIOS17
+        VStack {
+            Button("Show Scroll View") {
+                showModal = true
+            }
         }
-        
+        .fullScreenCover(isPresented: $showModal) {
+            if #available(iOS 17.0, *) {
+                scrollViewWithPagingVertical
+            } else {
+                scrollViewIOS16OrBelow
+            }
+        }
     }
     
     @available(iOS 17.0, *)
     @ViewBuilder
-    private var scrollViewIOS17Horizontal: some View {
+    private var scrollViewWithPagingHorizontal: some View {
         ScrollView(.horizontal) {
             LazyHStack {
                 ForEach(0..<10) { i in
@@ -36,23 +44,25 @@ struct ComponentScrollView: View {
     
     @available(iOS 17.0, *)
     @ViewBuilder
-    private var scrollViewIOS17Vertical: some View {
+    private var scrollViewWithPagingVertical: some View {
         ScrollView {
-            ForEach(0..<50) { i in
-                Text("Item \(i)")
+            ForEach(0..<10) { i in
+                Text("Card \(i)")
                     .font(.largeTitle)
                     .frame(maxWidth: .infinity)
                     .frame(height: 800)
-                    .background(.blue)
+                    .background(colors[i])
                     .foregroundStyle(.white)
-                    .clipShape(.rect(cornerRadius: 20))
+                    .clipShape(.rect(cornerRadius: 0))
+                    .scrollTargetLayout()
             }
         }
+        .frame(height: UIScreen.main.bounds.size.height)
         .scrollTargetBehavior(.paging)
     }
     
     @ViewBuilder
-    private var scrollViewPreIOS17: some View {
+    private var scrollViewIOS16OrBelow: some View {
         ScrollView {
             LazyHStack {
                 ForEach(0..<10) { i in
@@ -67,4 +77,42 @@ struct ComponentScrollView: View {
 
 #Preview {
     ComponentScrollView()
+}
+
+
+@available(iOS 17.0, *)
+struct CustomScrollTargetBehavior: ScrollTargetBehavior {
+    var targets = [0.0,
+                   932.0 * 1,
+                   932.0 * 2,
+                   932.0 * 3,
+                   932.0 * 4,
+                   932.0 * 5,
+                   932.0 * 6,
+                   932.0 * 7,
+                   932.0 * 8,
+                   932.0 * 9]
+    func updateTarget(_ target: inout ScrollTarget, context: TargetContext) {
+        if context.velocity.dy > 0 {
+            target.rect.origin.y = 820
+            print(target.anchor)
+        } else if context.velocity.dy < 0 {
+            target.rect.origin.y = 0
+        }
+        
+    }
+    
+    func nextTarget(_ value: CGFloat) -> CGFloat {
+        for i in targets {
+            if value < i {
+                return i
+            }
+        }
+        return 0.0
+    }
+}
+
+@available(iOS 17.0, *)
+extension ScrollTargetBehavior where Self == CustomScrollTargetBehavior {
+    static var custom: CustomScrollTargetBehavior { .init() }
 }
