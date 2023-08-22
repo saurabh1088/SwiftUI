72657f787434e4a7661b497519cf7a7f742afb56
Saurabh Verma <saurabh1088@gmail.com>
2023-08-22 22:47:21 +0530

    Fixed issue with camera not coming when arscn view opens up.

--- SwiftUILearnings/AugmentedReality/AugmentedRealityContainerView.swift
+++ SwiftUILearnings/AugmentedReality/AugmentedRealityContainerView.swift
@@ -9,11 +9,14 @@ import Foundation
 import SwiftUI
 import ARKit
 
+// TODO: Check this one https://www.gfrigerio.com/arkit-in-a-swiftui-app/
 struct AugmentedRealityContainerView: UIViewRepresentable {
     typealias UIViewType = ARSCNView
     
     func makeUIView(context: Context) -> ARSCNView {
         let augmentedRealitySceneView = ARSCNView(frame: .zero)
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
         let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
         
         let boxNode = SCNNode()
