8ae2a123dbcd74851f0595e05fec084be3c82d50
Saurabh Verma <saurabh1088@gmail.com>
2023-09-21 23:25:54 +0530

    Sample test to check animation.

--- SwiftUILearnings/TestPitch/TestPitchView.swift
+++ SwiftUILearnings/TestPitch/TestPitchView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct TestPitchView: View {
     @State private var animationWithDispatchTest = false
     @State private var offset = 0.0
+    @State private var showSampleTestPitchView = false
     
     var body: some View {
         VStack(spacing: 24) {
@@ -23,6 +24,12 @@ struct TestPitchView: View {
             } label: {
                 Text("Play")
             }
+            
+            Button {
+                showSampleTestPitchView.toggle()
+            } label: {
+                Text("Sample Pitch Fullscreen")
+            }
         }
         .onChange(of: animationWithDispatchTest) { newValue in
             DispatchQueue.main.async {
@@ -31,9 +38,40 @@ struct TestPitchView: View {
                 }
             }
         }
+        .fullScreenCover(isPresented: $showSampleTestPitchView) {
+            SampleTestPitchView(animate: $animationWithDispatchTest)
+        }
     }
 }
 
 #Preview {
     TestPitchView()
+}
+
+struct SampleTestPitchView: View {
+    @Binding var animate: Bool
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        VStack(spacing: 24) {
+            Image(systemName: "smiley")
+                .resizable()
+                .frame(width: 50, height: 50)
+            
+            Text("Sample Test Pitch View")
+                .font(.system(size: 20))
+            
+            Button {
+                animate.toggle()
+            } label: {
+                Text("Animate")
+            }
+            
+            Button {
+                presentationMode.wrappedValue.dismiss()
+            } label: {
+                Text("Dismiss")
+            }
+        }
+    }
 }
