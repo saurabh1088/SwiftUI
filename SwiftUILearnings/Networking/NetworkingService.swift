5ef5510be518c726086c85e558869c70fc448ee0
Saurabh Verma <saurabh1088@gmail.com>
2023-11-05 11:30:32 +0530

    Continued changes for networking.

--- SwiftUILearnings/Networking/NetworkingService.swift
+++ SwiftUILearnings/Networking/NetworkingService.swift
@@ -19,3 +19,25 @@ protocol NetworkingServiceProvider {
     func load(request: T, completion: @escaping (Result<Data>) -> Void)
     func load(request: T, decodedTo: U.Type, completion: @escaping (Result<U>) -> Void)
 }
+
+class NetworkingService<T: ServiceRequest, U: Codable> {
+    var urlSession: URLSession
+    
+    init(urlSession: URLSession = URLSession.shared) {
+        self.urlSession = urlSession
+    }
+    
+    func load(request: T, completion: @escaping (Result<Data>) -> Void) {
+        
+    }
+}
+
+extension NetworkingService {
+    private func makeRequest(_ request: URLRequest, 
+                             onQueue: DispatchQueue = DispatchQueue.main,
+                             completion: @escaping (Result<U>) -> Void) {
+        urlSession.dataTask(with: request) { data, response, error in
+            
+        }
+    }
+}
