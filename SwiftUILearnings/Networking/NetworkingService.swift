6a92c382e97b246ec508eac380df718a575def94
Saurabh Verma <saurabh1088@gmail.com>
2023-11-05 21:15:15 +0530

    Added NetworkingService functions to make requests and receive response.

--- SwiftUILearnings/Networking/NetworkingService.swift
+++ SwiftUILearnings/Networking/NetworkingService.swift
@@ -28,16 +28,41 @@ class NetworkingService<T: ServiceRequest, U: Codable> {
     }
     
     func load(request: T, completion: @escaping (Result<Data>) -> Void) {
-        
+        makeRequest(request.urlRequest, completion: completion)
+    }
+    
+    func load(request: T, decodedTo: U.Type, completion: @escaping (Result<U>) -> Void) {
+        makeRequest(request.urlRequest) { result in
+            switch result {
+            case .success(let data):
+                let decoder = JSONDecoder()
+                do {
+                    let response = try decoder.decode(decodedTo, from: data)
+                    completion(.success(response))
+                } catch {
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
     }
 }
 
 extension NetworkingService {
     private func makeRequest(_ request: URLRequest, 
                              onQueue: DispatchQueue = DispatchQueue.main,
-                             completion: @escaping (Result<U>) -> Void) {
+                             completion: @escaping (Result<Data>) -> Void) {
         urlSession.dataTask(with: request) { data, response, error in
-            
-        }
+            if let error = error {
+                onQueue.async {
+                    completion(.failure(error))
+                }
+            } else if let data = data {
+                onQueue.async {
+                    completion(.success(data))
+                }
+            }
+        }.resume()
     }
 }
