630406719144479b465fa4efb9e0945c1064ccce
Saurabh Verma <saurabh1088@gmail.com>
2023-11-05 09:29:18 +0530

    Added extension for ServiceRequest providing default functionalities.

--- SwiftUILearnings/Networking/ServiceRequest.swift
+++ SwiftUILearnings/Networking/ServiceRequest.swift
@@ -13,3 +13,26 @@ protocol ServiceRequest {
     var params: [String: Any]? { get }
     var httpMethod: HTTPMethods { get }
 }
+
+extension ServiceRequest {
+    var urlRequest: URLRequest {
+        guard let url = self.url else { fatalError("Error") }
+        var request = URLRequest(url: url)
+        request.httpMethod = httpMethod.rawValue
+        return request
+    }
+    
+    var url: URL? {
+        var urlComponents = URLComponents(string: baseURL)
+        urlComponents?.path = path
+        
+        if httpMethod == .GET {
+            guard let queryParams = params as? [String: String] else {
+                fatalError("Error")
+            }
+            urlComponents?.queryItems = queryParams.map({ URLQueryItem(name: $0.key, value: $0.value) })
+        }
+        
+        return urlComponents?.url
+    }
+}
