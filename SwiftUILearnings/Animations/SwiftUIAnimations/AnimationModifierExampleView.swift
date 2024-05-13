9c75c7c76d37603945abe674259863b1457cb5d2
Saurabh Verma <saurabh1088@gmail.com>
2024-05-13 19:06:52 +0530

    Updated dependency for swift-ui-views-helper to get generic value selectable view. Updated animation example view to use selectable view.

--- SwiftUILearnings/Animations/SwiftUIAnimations/AnimationModifierExampleView.swift
+++ SwiftUILearnings/Animations/SwiftUIAnimations/AnimationModifierExampleView.swift
@@ -8,21 +8,60 @@
 import SwiftUI
 import SwiftUIViewsHelper
 
+// TODO: Fix issue of navigation link opening and dismissing automatically the first time
+// TODO: Animation type when changed need to reset back the position and start afresh
+// TODO: Create a viewmodel for AnimationModifierExampleView
+enum AnimationType: String, CaseIterable {
+    case defaultAnimation = "Default"
+    case easeIn = "Ease In"
+    case easeOut = "Ease Out"
+    case easeInOut = "Ease In Out"
+    case interactiveSpring = "Interactive Spring"
+    
+    static var allAnimationTypes: [String] {
+        Self.allCases.map({ $0.rawValue })
+    }
+    
+    static func animationModifierValueFor(_ type: String) -> Animation {
+        switch type {
+        case AnimationType.defaultAnimation.rawValue:
+            return Animation.default
+        case AnimationType.easeIn.rawValue:
+            return Animation.easeIn
+        case AnimationType.easeOut.rawValue:
+            return Animation.easeOut
+        case AnimationType.easeInOut.rawValue:
+            return Animation.easeInOut
+        case AnimationType.interactiveSpring.rawValue:
+            return Animation.interactiveSpring
+        default:
+            return Animation.default
+        }
+    }
+}
+
 struct AnimationModifierExampleView: View {
     @State private var animate = false
+    @State private var selection: String?
     
     private var screenWidth: CGFloat {
         UIScreen.main.bounds.size.width
     }
     
+    private var animationType: Animation {
+        AnimationType.animationModifierValueFor(selection ?? AnimationType.defaultAnimation.rawValue)
+    }
+    
     var body: some View {
         VStack {
             animatingView
+            if #available(iOS 17.0, *) {
+                SelectableValueView(label: "Choose",
+                                    values: AnimationType.allAnimationTypes,
+                                    selection: $selection)
+            }
+            animationSelectorButton
         }
-        .sheet(isPresented: .constant(true), content: {
-            animationSelectorSheet
-                .presentationDetents([.height(100), .medium])
-        })
     }
     
     @ViewBuilder
@@ -31,32 +70,12 @@ struct AnimationModifierExampleView: View {
             Text("🚴")
                 .font(.system(size: 40))
                 .offset(x: animate ? 0 : screenWidth)
-                .animation(.default, value: animate)
-            
-            Text("🚴")
-                .font(.system(size: 40))
-                .offset(x: animate ? 0 : screenWidth)
-                .animation(.easeIn, value: animate)
-            
-            Text("🚴")
-                .font(.system(size: 40))
-                .offset(x: animate ? 0 : screenWidth)
-                .animation(.easeOut, value: animate)
-            
-            Text("🚴")
-                .font(.system(size: 40))
-                .offset(x: animate ? 0 : screenWidth)
-                .animation(.easeInOut, value: animate)
-            
-            Text("🚴")
-                .font(.system(size: 40))
-                .offset(x: animate ? 0 : screenWidth)
-                .animation(.interactiveSpring, value: animate)
+                .animation(animationType, value: animate)
         }
     }
     
     @ViewBuilder
-    private var animationSelectorSheet: some View {
+    private var animationSelectorButton: some View {
         VStack {
             Button {
                 animate.toggle()
