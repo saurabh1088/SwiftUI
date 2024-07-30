0f1df6971f3f3d2fee02911ac1bae709239d4629
Saurabh Verma <saurabh1088@gmail.com>
2024-07-30 22:57:44 +0530

    Updated GeometryReader example for parent child to show behaviour when parent geometry changes.

--- SwiftUILearnings/GeometryReader/ExampleViews/GeometryReaderParentChildExampleView.swift
+++ SwiftUILearnings/GeometryReader/ExampleViews/GeometryReaderParentChildExampleView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 /// Here in this example we have a parent and child view hierarchy. What is shown here is that when `GeometryReader`
-/// is used then the 
+/// is used for the child view, the child will get to know the geometry of it's parent through `GeometryProxy`
+/// passed in the closure of `GeometryReader`. This proxy can be used to read the geometry of parent view
+/// and used further.
+/// Here in this example, the child view uses the geometry to make itself half of the size of the parent.
 struct GeometryReaderParentChildExampleView: View {
+    @State private var dimension: CGFloat = 300
     
     var body: some View {
         VStack {
             parentView
+            
+            HStack {
+                updateDimension(value: 100)
+                updateDimension(value: 200)
+                updateDimension(value: 300)
+            }
         }
     }
     
@@ -21,7 +31,7 @@ struct GeometryReaderParentChildExampleView: View {
         VStack {
             childView
         }
-        .frame(width: 300, height: 300)
+        .frame(width: dimension, height: dimension)
         .background(Color.red)
     }
     
@@ -31,6 +41,18 @@ struct GeometryReaderParentChildExampleView: View {
                 Color.blue
             }
             .frame(width: proxy.size.width/2, height: proxy.size.height/2)
+        }
+    }
+    
+    @ViewBuilder private func updateDimension(value: CGFloat) -> some View {
+        Button {
+            withAnimation {
+                dimension = value
+            }
+        } label: {
+            Text("\(value)")
+                .background(Color.red)
+                .foregroundStyle(Color.white)
         }
     }
 }
