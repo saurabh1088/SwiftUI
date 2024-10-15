87b2e7e9d224e8f012fdfd32d4f2ced118cb853f
Saurabh Verma <saurabh1088@gmail.com>
2024-10-15 22:14:24 +0530

    Modified to explore how to handle dynamic ui elements in table.

--- SwiftUILearnings/Components/Tables/RowColumnTableView.swift
+++ SwiftUILearnings/Components/Tables/RowColumnTableView.swift
@@ -8,27 +8,45 @@
 import SwiftUI
 
 struct Employee: Identifiable {
-    let id = UUID()
+    let id: Int
     let name: String
     let role: String
     let power: String
+    var shouldShowPower: Bool
 }
 
 struct RowColumnTableView: View {
     
     @State private var employees: [Employee] = [
-        Employee(name: "Batman", role: "Dark Knight", power: "World's Greatest detective"),
-        Employee(name: "Superman", role: "Man of Steel", power: "Most Powerful Man in the World"),
-        Employee(name: "Aquaman", role: "King of Atlantis", power: "Swimming is his superpower")
+        Employee(id: 0,
+                 name: "Batman",
+                 role: "Dark Knight",
+                 power: "World's Greatest detective",
+                 shouldShowPower: true),
+        Employee(id: 1,
+                 name: "Superman",
+                 role: "Man of Steel",
+                 power: "Most Powerful Man in the World",
+                 shouldShowPower: false),
+        Employee(id: 2,
+                 name: "Aquaman",
+                 role: "King of Atlantis",
+                 power: "Swimming is his superpower",
+                 shouldShowPower: false)
     ]
     
     var body: some View {
+        let binding = Binding(
+            get: { self.employees },
+            set: { self.employees = $0 }
+        )
+        
         Table(employees) {
             TableColumn("Name", value: \.name)
             TableColumn("Role", value: \.role)
             TableColumn("Power", value: \.power)
             TableColumn("Test") { employee in
-                Toggle("Should Show", isOn: .constant(true))
+                Toggle("Should Show", isOn: binding[employee.id].shouldShowPower)
             }
         }
     }
