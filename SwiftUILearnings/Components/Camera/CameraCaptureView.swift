a4a7f8e7f37acfc2536972c87472244a21324bb3
Saurabh Verma <saurabh1088@gmail.com>
2024-10-18 20:27:21 +0530

    Addressed TODO comment to refactor.

--- SwiftUILearnings/Components/Camera/CameraCaptureView.swift
+++ SwiftUILearnings/Components/Camera/CameraCaptureView.swift
@@ -6,8 +6,8 @@
 //
 
 import SwiftUI
+import SwiftUIViewsHelper
 
-// TODO: Refactor this and move into independent files.
 struct CameraCaptureView: View {
     @State private var image: UIImage?
     @State private var showCamera = false
@@ -24,12 +24,13 @@ struct CameraCaptureView: View {
                     let editedImage = addTextToImage(image: image, text: customText)
                     saveImageToPhotos(image: editedImage)
                 }
-                .padding()
+                .buttonStyle(.fullScreenWide)
+                
             } else {
                 Button("Take a Photo") {
                     showCamera = true
                 }
-                .padding()
+                .buttonStyle(.fullScreenWide)
             }
         }
         .sheet(isPresented: $showCamera) {
@@ -40,7 +41,7 @@ struct CameraCaptureView: View {
     
     private var customText: String {
         let formatter = DateFormatter()
-        formatter.dateFormat = "HH:mm:ss"
+        formatter.dateFormat = "EEEE, MMMM dd, yyyy h:mm a"
         return "😊 : \(formatter.string(from: Date()))"
     }
     
@@ -49,7 +50,7 @@ struct CameraCaptureView: View {
         return renderer.image { context in
             image.draw(at: CGPoint.zero)
             let textAttributes: [NSAttributedString.Key: Any] = [
-                .font: UIFont.systemFont(ofSize: 60),
+                .font: UIFont.systemFont(ofSize: 128),
                 .foregroundColor: UIColor.red
             ]
             let textRect = CGRect(x: 50, y: 50, width: image.size.width, height: image.size.height)
@@ -57,7 +58,6 @@ struct CameraCaptureView: View {
         }
     }
 
-    // Step 3: Saving the image to the device
     func saveImageToPhotos(image: UIImage) {
         UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
     }
@@ -65,38 +65,4 @@ struct CameraCaptureView: View {
 
 #Preview {
     CameraCaptureView()
-}
-
-
-struct CameraView: UIViewControllerRepresentable {
-    @Binding var image: UIImage?
-    @Environment(\.presentationMode) var presentationMode
-
-    func makeUIViewController(context: Context) -> UIImagePickerController {
-        let picker = UIImagePickerController()
-        picker.delegate = context.coordinator
-        picker.sourceType = .camera
-        return picker
-    }
-
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
-
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-
-    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-        let parent: CameraView
-
-        init(_ parent: CameraView) {
-            self.parent = parent
-        }
-
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            if let image = info[.originalImage] as? UIImage {
-                parent.image = image
-            }
-            parent.presentationMode.wrappedValue.dismiss()
-        }
-    }
 }
---
+++ SwiftUILearnings/Components/Camera/UIKitRepresentables/CameraView.swift
@@ -0,0 +1,43 @@
+//
+//  CameraView.swift
+//  SwiftUILearnings
+//
+//  Created by Saurabh Verma on 18/10/24.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+struct CameraView: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = .camera
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: CameraView
+
+        init(_ parent: CameraView) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.image = image
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
