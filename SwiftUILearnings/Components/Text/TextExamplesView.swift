ae9e593223b5c9e3f56ffb8b15273d7673bfdcc6
Saurabh Verma <saurabh1088@gmail.com>
2025-10-02 18:37:10 +0530

    Added more example for text concatenation.

--- SwiftUILearnings/Components/Text/TextExamplesView.swift
+++ SwiftUILearnings/Components/Text/TextExamplesView.swift
@@ -9,11 +9,35 @@ import SwiftUI
 
 struct TextExamplesView: View {
     var body: some View {
-        simpleConcatenation
+        VStack {
+            simpleConcatenation
+            styledText
+            emphasizedWord
+            starsInText
+        }
     }
     
     @ViewBuilder var simpleConcatenation: some View {
-        Text("Hello").bold() + Text(" World") + Text("!")
+        Text("Hello").bold()
+        + Text(" World") + Text("!")
+    }
+    
+    @ViewBuilder var styledText: some View {
+        Text("SwiftUI ").font(.title).foregroundColor(.blue)
+        + Text("Text ").italic().foregroundColor(.red)
+        + Text("Concatenation").underline()
+    }
+    
+    @ViewBuilder var emphasizedWord: some View {
+        Text("This is ")
+        + Text("important").bold().foregroundColor(.orange)
+        + Text("!")
+    }
+    
+    @ViewBuilder var starsInText: some View {
+        Text("Feature rating: ")
+        + Text(Image(systemName: "star.fill")).foregroundColor(.yellow)
+        + Text(" 5/5")
     }
 }
 
